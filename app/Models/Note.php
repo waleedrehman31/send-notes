d9fbdde63254ff1fda91b63503ba3155c61a3944
waleedrehman31 <waleedrehman31@gmail.com>
2024-09-15 16:34:54 +0500

    fresh laravel project

---
+++ app/Models/Note.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publishedNotes(User $user)
+    {
+        return $this->where('user_id', $user->id)->where('is_published', true)->get();
+    }
+}
